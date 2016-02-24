4be6a321a073af9881263e954a44fb22258f2629
kiri93 <mk.youssef@hotmail.com>
2016-02-24 18:59:24 +0100

    actually works now

--- main.py
+++ main.py
@@ -7,17 +7,26 @@ Created on Sun Feb 21 20:16:07 2016
 from scipy import *
 from pylab import *
 from mydeafdog import *
+import os
 
 import scipy.misc as sm
         
-        
-video = soundToImage('duck.wav',5)
+resolution = 5
+filename = 'duck.wav'
+name = filename[:-4]
+os.system('mkdir '+ name)
+
+video = soundToImage(filename,resolution)
 
 for k in range(len(video)):
     
     number = '{:05}'.format(k+1)
-    string = 'video/image' + number + '.png'
+    string = name + '/'+ name + number + '.png'
     
     M = sm.toimage(video[k],video[k].max(),video[k].min())
     sm.imsave(string,M)
-    
+
+fps = 44100/(2**resolution)**2
+makemovie = 'ffmpeg -f image2 -r {1} -i {0}/{0}%05d.png -vcodec mpeg4 -y {0}/{0}.mp4'.format(name,fps)
+
+os.system(makemovie)
