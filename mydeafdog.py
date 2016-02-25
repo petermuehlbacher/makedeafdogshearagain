cf35c56d8b2705b7e2411c72f80ba0e4389f46e4
petermuehlbacher <peter@muehlbacher.me>
2016-02-25 08:12:33 +0100

    deleted the definition of resolution in pixelToImage() by accident in the last commit - revert that change

--- mydeafdog.py
+++ mydeafdog.py
@@ -10,78 +10,79 @@ from cmath import atanh
 
 import wave
 import struct
-import subprocess   
-   
+import subprocess
 
 
-def rescaling( x, c = 0.99, D = [-2**15,2**15-1], W = [0,255] ):      
-    
+
+def rescaling( x, c = 0.99, D = [-2**15,2**15-1], W = [0,255] ):
+
     w = W[1]-W[0]                           # lengthe of range
     d = D[1]-D[0]                           # length of domain
-    
+
     T = 2 * atanh(2*c/w-1)                  # upper bound where 100*c % of the limit can theoretically be achieved
     t = [(k-D[0])/d * 2*T - T for k in x]             # rescaling the domain, so it matches to [-T,T]
-    
+
     y = [((exp(k)/(1+exp(k))) * w + W[0])/c for k in t]
-    
+
     return y
 
-    
+
 def freqToCoeff(freq):                                  # muss noch normiert werden !!!
-    
+
     n = len(freq)
     coeff = zeros(n)
-    
+
     for k in range(1,(n//2)):
         coeff[n//2-k] = real(1j*(freq[k]-freq[-k]))
         coeff[n//2+k] = real(freq[k]+freq[-k])
     coeff[n//2] = real(freq[0])
     coeff[0] = real(freq[-(n//2)])
-    
+
     return coeff
-    
-    
-    
+
+
+
 
 def coeffToPixel(coeff):
     n = len(coeff)
-    
+
     pixel = [k/(n*(2.**8))+128 for k in coeff]
-    
+
     return pixel
-      
- 
+
+
 
 
 def pixelToImage(pixel):
     n = size(pixel)
     l = int(sqrt(n))
     M = zeros([l,l])
-    
+    resolution = int(log2(l))
+
     indexToX  = [None for i in xrange(l**2)]
     indexToY  = [None for i in xrange(l**2)]
-            
+
     for x in xrange(l):
         for y in xrange(l):
             i = pixelToIndex(x,y,resolution)
             indexToX[i] = x
             indexToY[i] = y
-        
-    
+
+
     for i in xrange(n):
-        M[[l-indexToY[i]-1],indexToX[i]] = pixel[i]    
+        M[[l-indexToY[i]-1],indexToX[i]] = pixel[i]
 
     return M
-    
+
 
 def freqToImage(freq):
-    
+
     coeff = freqToCoeff(freq)
     pixel = coeffToPixel(coeff)
     image = pixelToImage(pixel)
-    
+
     return image
-    
+
 
 
 
@@ -185,20 +186,20 @@ def pixelToIndex(x,y,resolution):
 def soundToImage(filename, resolution = 6):
     sample, framerate = soundToSample(filename)
     # resolution=6 yields chunks of ~1/10 seconds length with the framerate of 44100
-    
+
     if framerate != 44100:
         print('wrong framerate!')
         return 0
-    
+
     N = (2**resolution)**2
-    
+
     chunk_num = len(sample) // N
-    
+
     chunks = [sample[i*N:(i+1)*N] for i in range(chunk_num)]
-    
+
     video = []
-    
+
     for chunk in chunks:
         video.append(freqToImage(fft(chunk)))
-    
+
     return video
