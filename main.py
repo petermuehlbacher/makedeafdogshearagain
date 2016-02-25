a131a21990955b7785f88b1f05465a55e14ad70a
petermuehlbacher <peter@muehlbacher.me>
2016-02-25 08:11:21 +0100

    by default call a file that's present on Github

--- main.py
+++ main.py
@@ -9,15 +9,14 @@ from pylab import *
 from mydeafdog import *
 
 import scipy.misc as sm
-        
-        
-video = soundToImage('duck.wav',5)
+
+
+video = soundToImage('whistle.wav',5)
 
 for k in range(len(video)):
-    
+
     number = '{:05}'.format(k+1)
     string = 'video/image' + number + '.png'
-    
+
     M = sm.toimage(video[k],video[k].max(),video[k].min())
     sm.imsave(string,M)
-    
