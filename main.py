8507747e97c7601b5733f0ed02e02e17166c9785
kiri93 <mk.youssef@hotmail.com>
2016-02-25 09:30:54 +0100

    fixed strange occurrences of random lines

--- main.py
+++ main.py
@@ -10,24 +10,17 @@ from mydeafdog import *
 import os
 
 import scipy.misc as sm
-<<<<<<< HEAD
         
 resolution = 5
-filename = 'duck.wav'
+filename = '440-660hz.wav'
 name = filename[:-4]
 os.system('mkdir '+ name)
 
 video = soundToImage(filename,resolution)
-=======
-
-
-video = soundToImage('whistle.wav',5)
->>>>>>> origin/master
 
 for k in range(len(video)):
-
+    
     number = '{:05}'.format(k+1)
-<<<<<<< HEAD
     string = name + '/'+ name + number + '.png'
     
     M = sm.toimage(video[k],video[k].max(),video[k].min())
@@ -37,9 +30,3 @@ fps = 44100/(2**resolution)**2
 makemovie = 'ffmpeg -f image2 -r {1} -i {0}/{0}%05d.png -vcodec mpeg4 -y {0}/{0}.mp4'.format(name,fps)
 
 os.system(makemovie)
-=======
-    string = 'video/image' + number + '.png'
-
-    M = sm.toimage(video[k],video[k].max(),video[k].min())
-    sm.imsave(string,M)
->>>>>>> origin/master
